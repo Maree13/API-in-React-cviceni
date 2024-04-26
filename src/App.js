04c9c1bb00c3d1d0a6da2adacab81a0df7387168
Mariana Sarovic <sarovicmariana@gmail.com>
2024-04-26 19:58:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// API
 
-function App() {
+import { useState, useEffect } from 'react';
+
+const App = () => {
+  const [quote, setQuote] = useState('vychozi text');
+  const [loading, setLoading] = useState(true);
+  const url = 'https://api.kanye.rest/';
+
+  const getQuote = async () => {
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    const finalQuote = data.quote;
+
+    setQuote(finalQuote);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  if (loading) {
+    return <h1>Nacitani dat...</h1>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{quote}</h2>
     </div>
   );
-}
+};
 
 export default App;
